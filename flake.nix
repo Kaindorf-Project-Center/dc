d27011adfd4378b6946968ba3a5ea469750cc854
Flokkq <webcla21@htl-kaindorf.at>
2024-12-25 12:57:33 +0100

    chore(ci): automaticly run `pre-commit install` when entering environment
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- flake.nix
+++ flake.nix
@@ -3,27 +3,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs =
-    { systems, nixpkgs, ... }@inputs:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            pre-commit
-            act
-            nodejs_22
+  outputs = {
+    systems,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pre-commit
+          act
+          nodejs_22
 
-            bun
+          bun
 
-            eslint
-            nodePackages.prettier
-            nodePackages.typescript
-            nodePackages.typescript-language-server
-          ];
-        };
-      });
-    };
+          eslint
+          nodePackages.prettier
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+        ];
+
+        # Automatically run `pre-commit install` when entering the shell
+        shellHook = ''
+          echo "Installing pre-commit hooks..."
+          pre-commit install
+        '';
+      };
+    });
+  };
 }
