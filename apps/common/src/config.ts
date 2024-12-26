7041bb0ba19ac0d7a7d09294fabf41cbaa11c08c
Flokkq <webcla21@htl-kaindorf.at>
2024-12-26 16:36:09 +0100

    feat(common): add config that can be imported from all repos
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

---
+++ apps/common/src/config.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const ConfigSchema = z.object({
+  DISCORD_TOKEN: z.string().nonempty("DISCORD_TOKEN is required"),
+  MICROSOFT_CLIENT_ID: z.string().nonempty("MICROSOFT_CLIENT_ID is required"),
+  MICROSOFT_CLIENT_SECRET: z
+    .string()
+    .nonempty("MICROSOFT_CLIENT_SECRET is required"),
+  MICROSOFT_REDIRECT_URI: z
+    .string()
+    .url("MICROSOFT_REDIRECT_URI must be a valid URL"),
+  MICROSOFT_TENANT_ID: z.string().nonempty("MICROSOFT_TENANT_ID is required"),
+});
+
+export const config = ConfigSchema.parse(process.env);
--- apps/discord-bot/src/index.ts
+++ apps/discord-bot/src/index.ts
@@ -1,15 +1,14 @@
 // Require the necessary discord.js classes
 import { Client, GatewayIntentBits } from "discord.js";
 import { loadEvents } from "./loader";
-
-const token = process.env.DISORD_TOKEN;
+import { config } from "@monorepo/common";
 
 // Create a new client instance
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
 loadEvents(client);
 
 // Log in to Discord with your client's token
-client.login(token);
+client.login(config.DISCORD_TOKEN);
