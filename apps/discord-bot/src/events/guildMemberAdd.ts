26c26b5c333658510c6448619817d0acb366c9c4
svenbla <62405862+svenbla@users.noreply.github.com>
2025-01-08 15:55:35 +0100

    add msg edit on button time

--- apps/discord-bot/src/events/guildMemberAdd.ts
+++ apps/discord-bot/src/events/guildMemberAdd.ts
@@ -1,13 +1,23 @@
-import { GuildMember, Events, EmbedBuilder } from "discord.js";
+import {
+  GuildMember,
+  Events,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  MessageComponentInteraction,
+} from "discord.js";
 import { randomBytes } from "node:crypto";
 import { config } from "common";
 import type { Event } from "../types";
+import { log } from "node:console";
 
 const event: Event<typeof Events.GuildMemberAdd> = {
   name: Events.GuildMemberAdd,
   once: false,
   async execute(member: GuildMember) {
     console.log(member.displayName);
+
     const dmChannel = await member.createDM();
     const state = randomBytes(16).toString("hex"); // For CSRF protection
     const authUrl = `https://login.microsoftonline.com/${
@@ -18,6 +28,7 @@ const event: Event<typeof Events.GuildMemberAdd> = {
       config.MICROSOFT_REDIRECT_URI
     )}&response_mode=query&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&state=${state}`;
 
+    // Create the embed and button
     const embed = new EmbedBuilder()
       .setTitle("Authentication Required")
       .setDescription(
@@ -26,7 +37,55 @@ const event: Event<typeof Events.GuildMemberAdd> = {
       .setURL(authUrl)
       .setColor("Blue");
 
-    await dmChannel.send({ embeds: [embed] });
+    const verifyButton = new ButtonBuilder()
+      .setCustomId(`verify-${member.id}`)
+      .setLabel("Verify")
+      .setStyle(ButtonStyle.Primary);
+
+    const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      verifyButton
+    );
+
+    // Send the initial message
+    const message = await dmChannel.send({
+      embeds: [embed],
+      components: [actionRow],
+    });
+
+    // Define the filter for the collector
+    const filter = (interaction: MessageComponentInteraction) =>
+      interaction.isButton() && interaction.customId === `verify-${member.id}`;
+
+    // Create the message component collector
+    const collector = dmChannel.createMessageComponentCollector({
+      filter,
+      time: 60000 * 5, // 5 min timeout for the button
+    });
+
+    // Handle the end of the collector
+    collector.on("end", async (collected, reason) => {
+      if (reason === "time") {
+        const embed = new EmbedBuilder()
+          .setTitle("Authentication Expired")
+          .setDescription(
+            "The Authentication has expired please use `/verify` to try again"
+          )
+          .setColor("Red");
+
+        await message.edit({
+          embeds: [embed], // Add the new embed
+          components: [], // Empty array to remove the buttons
+        });
+
+        console.log("Message edited after timeout.");
+      }
+    });
+
+    collector.on("collect", async (interaction) => {
+      if (!interaction.deferred) await interaction.deferUpdate();
+
+      console.log(`${member.displayName} clicked the verify button.`);
+    });
   },
 };
 
